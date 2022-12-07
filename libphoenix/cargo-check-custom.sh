818c0555507767b4a76ca0ac3ff8677e808bd19c
Jeremy Davis <jeremydavis519@gmail.com>
2022-12-07 14:17:28 -0600

    libphoenix: Make `cargo-check-custom.sh` find the target specification.

--- libphoenix/cargo-check-custom.sh
+++ libphoenix/cargo-check-custom.sh
@@ -2,6 +2,9 @@
 
 # TODO: Check every permutation of targets and available features.
 
+dir="`dirname "$0"`"
+
+RUST_TARGET_PATH="`realpath "$dir/.."`" \
 cargo check -Z build-std --target=aarch64-phoenix --features=global-allocator "$@" && \
 cargo check -Z build-std --target=aarch64-phoenix --features=kernelspace "$@" && \
 cargo check -Z build-std --target=aarch64-phoenix --features=no-start "$@" && \
