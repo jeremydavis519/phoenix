d576167783a0125a0b17bfca42c0a2ec9e648c5d
Jeremy Davis <jeremydavis519@gmail.com>
2023-10-02 12:37:23 -0500

    libphoenix: Add a userspace API for the `process_spawn` system call.

--- libphoenix/src/syscall.rs
+++ libphoenix/src/syscall.rs
@@ -18,14 +18,27 @@
 
 //! This module defines a function to encapsulate each system call the Phoenix kernel understands.
 
+#[cfg(not(feature = "kernelspace"))]
 use {
+    alloc::vec::Vec,
     core::{
         arch::asm,
-        convert::TryFrom,
         mem::{self, MaybeUninit},
     },
+    crate::{
+        ipc::FileDescriptor,
+        process::ProcessId,
+        serde::{DefaultSerializer, Serializer, SerializeError, serialize_object},
+    },
 };
+use core::convert::TryFrom;
 
+#[cfg(not(feature = "kernelspace"))]
+const DEFAULT_STACK_SIZE: usize = 0x0001_0000;
+#[cfg(not(feature = "kernelspace"))]
+const DEFAULT_PRIORITY: u8 = 10;
+
+#[cfg(not(feature = "kernelspace"))]
 /// Ends the currently running thread with the given status.
 ///
 /// This function is exactly equivalent to [`process_exit`] if the process has only one running
@@ -59,6 +72,7 @@ pub extern "C" fn thread_exit(status: i32) -> ! {
     }
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Halts the currently running thread for the given duration.
 ///
 /// The given duration is a lower bound. The thread may (and likely will) be inactive for slightly
@@ -84,6 +98,7 @@ pub extern "C" fn thread_sleep(milliseconds: u64) {
     }
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Spawns a new thread with the given entry point, priority, and stack size.
 ///
 /// This function is not marked as public because the [`Thread`] type provides a more idiomatic
@@ -95,6 +110,7 @@ pub(crate) fn thread_spawn(entry_point: fn(), priority: u8, stack_size: usize) -
     thread_spawn_ffi(call_rust_abi, entry_point as usize, priority, stack_size)
 }
 
+#[cfg(not(feature = "kernelspace"))]
 #[export_name = "thread_spawn"]
 extern "C" fn thread_spawn_ffi(
     entry_point: extern "C" fn(usize) -> !,
@@ -117,12 +133,14 @@ extern "C" fn thread_spawn_ffi(
     handle
 }
 
+#[cfg(not(feature = "kernelspace"))]
 extern "C" fn call_rust_abi(f: usize) -> ! {
     let f = unsafe { mem::transmute::<_, fn()>(f) };
     f();
     thread_exit(0);
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Ends the currently running process with the given status.
 ///
 /// Every thread in the process is immediately terminated.
@@ -148,6 +166,72 @@ pub extern "C" fn process_exit(status: i32) -> ! {
     }
 }
 
+#[cfg(not(feature = "kernelspace"))]
+/// Spawns a new process from the executable file at the given path.
+///
+/// This function works at about the same level as [`posix_spawn`], so it isn't quite as versatile
+/// as `fork` and `exec`. But for the usual case of running another program, this should be quite
+/// sufficient.
+///
+/// # Returns
+/// The ID of the newly spawned process, or `Err` if it couldn't be spawned.
+///
+/// [`posix_spawn`]: https://pubs.opengroup.org/onlinepubs/9699919799/functions/posix_spawn.html
+pub fn process_spawn(path: &str, argv: &[&[u8]], file_descriptors: &[FileDescriptor]) -> Result<ProcessId, ProcessSpawnError> {
+    // Serialize the data.
+    let mut data = Vec::new();
+    data.extend_from_slice(&DEFAULT_STACK_SIZE.to_ne_bytes()[..]);
+    data.push(DEFAULT_PRIORITY);
+    data.extend_from_slice(&[0; 7][..]);
+    data.extend_from_slice(&argv.len().to_ne_bytes()[..]);
+    data.extend_from_slice(&(argv as *const [&[u8]]).expose_addr().to_ne_bytes()[..]);
+
+    let argv_strings_size: usize = argv.iter().map(|s| s.len() + 1).sum();
+    data.extend_from_slice(&argv_strings_size.to_ne_bytes()[..]);
+
+    for i in 0 .. argv.len() {
+        data.extend_from_slice(&argv[i].len().to_ne_bytes()[..]);
+    }
+
+    let mut serializer = DefaultSerializer::new();
+    serialize_object!(&mut serializer, {
+        "fds" => |serializer| serializer.list(file_descriptors)
+    }).map_err(|e| ProcessSpawnError::SerializeError(e))?;
+    data.append(&mut serializer.finish());
+
+    // Spawn the process.
+    let (pid, errno);
+    unsafe {
+        asm!(
+            "svc 0x0101",
+            in("x2") path.as_bytes() as *const [u8] as *const u8 as usize,
+            in("x3") path.len(),
+            in("x4") &data[..] as *const [u8] as *const u8 as usize,
+            in("x5") data.len(),
+            lateout("x0") pid,
+            lateout("x1") errno,
+            options(nomem, nostack, preserves_flags),
+        );
+    }
+
+    ProcessId::new(pid).ok_or(ProcessSpawnError::Errno(errno))
+}
+
+#[cfg(not(feature = "kernelspace"))]
+/// An error that can occur as a result of trying and failing to spawn a process.
+#[derive(Debug)]
+pub enum ProcessSpawnError {
+    /// An error occurred when serializing the data for the process.
+    SerializeError(SerializeError),
+
+    /// An error occurred when trying to start the process.
+    ///
+    /// Compare the contained value to variants of [`Errno`](crate::posix::errno::Errno) for more
+    /// information about the error.
+    Errno(u64),
+}
+
+#[cfg(not(feature = "kernelspace"))]
 /// Looks up a physical device by name and claims ownership of it if it exists.
 ///
 /// This function is intended for use only by `libdriver`, which builds further abstractions on
@@ -163,6 +247,7 @@ pub fn device_claim(name: &str) -> usize {
     device_claim_ffi(name as *const str as *const u8, name.len())
 }
 
+#[cfg(not(feature = "kernelspace"))]
 #[export_name = "device_claim"]
 extern "C" fn device_claim_ffi(name: *const u8, len: usize) -> usize {
     let device_addr: usize;
@@ -178,6 +263,7 @@ extern "C" fn device_claim_ffi(name: *const u8, len: usize) -> usize {
     device_addr
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Frees a block of memory that was allocated by a system call.
 ///
 /// # Aborts the thread
@@ -192,6 +278,7 @@ pub unsafe extern "C" fn memory_free(ptr: *mut MaybeUninit<u8>) {
     );
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Allocates a new block of virtual memory with the given size and alignment.
 ///
 /// This is only meant for use by the global allocator. Other userspace code should allocate
@@ -214,6 +301,7 @@ pub extern "C" fn memory_alloc(size: usize, align: usize) -> *mut MaybeUninit<u8
     addr
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Allocates a new block of memory with the given size and alignment. The memory is guaranteed
 /// to remain resident at the same address in physical memory until it is freed, and it is
 /// guaranteed that the highest physical address in the block fits within a `max_bits`-bit
@@ -256,6 +344,7 @@ pub extern "C" fn memory_alloc_phys(size: usize, align: usize, max_bits: usize) 
     VirtPhysAddr { virt, phys }
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Allocates a new block of shared virtual memory with the given size.
 ///
 /// This is a low-level primitive for inter-process communication and should probably not be used
@@ -285,6 +374,7 @@ pub extern "C" fn memory_alloc_shared(size: usize) -> *mut MaybeUninit<u8> {
     addr
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Requests access to a block of shared virtual memory.
 ///
 /// This is a low-level primitive for inter-process communication and should probably not be used
@@ -319,6 +409,7 @@ pub extern "C" fn memory_access_shared(orig_addr: usize, size: usize) -> *mut Ma
     addr
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Returns the number of bytes in a page.
 ///
 /// A page is the smallest unit of memory that the kernel will allocate for a userspace program, and
@@ -342,6 +433,7 @@ pub extern "C" fn memory_page_size() -> usize {
     page_size
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Returns the current time as a UNIX timestamp (i.e. whole seconds since 1970-01-01T00:00:00).
 ///
 /// # Example
@@ -370,6 +462,7 @@ pub extern "C" fn time_now_unix() -> u64 {
     unix_time
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Returns the current time in relation to the UNIX epoch, but measured in nanoseconds
 /// (i.e. nanoseconds since 1970-01-01T00:00:00.00).
 ///
@@ -399,6 +492,7 @@ pub extern "C" fn time_now_unix_nanos() -> u64 {
     unix_time
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Resets the kernel's internal performance profile. This should be done right before the code
 /// that will be measured.
 pub fn time_reset_kernel_profile() {
@@ -410,7 +504,7 @@ pub fn time_reset_kernel_profile() {
     }
 }
 
-
+#[cfg(not(feature = "kernelspace"))]
 /// *Note:* This function is not designed to be used directly. Applications should use
 /// [`crate::profiler::kernel_probes`] instead.
 ///
@@ -432,6 +526,7 @@ pub fn time_view_kernel_profile() -> usize {
     profile_addr
 }
 
+#[cfg(not(feature = "kernelspace"))]
 /// Used for packaging a virtual address and a physical address into a single return value.
 #[derive(Debug)]
 #[repr(C)]
@@ -442,6 +537,7 @@ pub struct VirtPhysAddr {
     pub phys: usize,
 }
 
+#[cfg(not(feature = "kernelspace"))]
 impl VirtPhysAddr {
     /// Returns `true` if the address is null.
     pub fn is_null(&self) -> bool {
