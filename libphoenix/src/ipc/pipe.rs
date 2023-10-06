5f452b05b870a865892be28070cb97668184b9cd
Jeremy Davis <jeremydavis519@gmail.com>
2023-10-06 14:43:51 -0500

    libphoenix: Increment reader and writer counts for new pipes in FFI.

--- libphoenix/src/ipc/pipe.rs
+++ libphoenix/src/ipc/pipe.rs
@@ -70,8 +70,8 @@ mod ffi {
     extern "C" fn pipe_new(reader: *mut *mut PipeReader, writer: *mut *mut PipeWriter) -> i8 {
         let Ok(pipe) = Pipe::try_new() else { return -1 };
         let Ok(pipe) = Arc::try_new(pipe) else { return -1 };
-        let Ok(boxed_reader) = Box::try_new(PipeReader { pipe: pipe.clone() }) else { return -1 };
-        let Ok(boxed_writer) = Box::try_new(PipeWriter { pipe }) else { return -1 };
+        let Ok(boxed_reader) = Box::try_new(pipe.clone().new_reader()) else { return -1 };
+        let Ok(boxed_writer) = Box::try_new(pipe.new_writer()) else { return -1 };
         unsafe {
             *reader = Box::into_raw(boxed_reader);
             *writer = Box::into_raw(boxed_writer);
